d598f8c7a8fe95e2688f823b8a42c4de70e6f46e
matte21 <matteoolivi7@gmail.com>
2025-09-22 20:53:03 -0400

    No longer enforce fine-grained cgroups

--- pkg/kubelet/cm/internal_container_lifecycle_linux.go
+++ pkg/kubelet/cm/internal_container_lifecycle_linux.go
@@ -53,9 +53,9 @@ func preCreateContainerWUnifiedMgr(fmm *farmemtopologymanager.Manager, pod *v1.P
 	}
 
 	var cpusetMems []string
-	containerConfig.Linux.Resources.MaxPerMemInBytes = make(map[uint64]uint64, len(a.PerNUMANodeMemBytes))
-	for node, maxMemBytes := range a.PerNUMANodeMemBytes {
-		containerConfig.Linux.Resources.MaxPerMemInBytes[uint64(node)] = maxMemBytes
+	//containerConfig.Linux.Resources.MaxPerMemInBytes = make(map[uint64]uint64, len(a.PerNUMANodeMemBytes))
+	for node := range a.PerNUMANodeMemBytes {
+		//containerConfig.Linux.Resources.MaxPerMemInBytes[uint64(node)] = maxMemBytes
 		cpusetMems = append(cpusetMems, strconv.Itoa(node))
 	}
 	containerConfig.Linux.Resources.CpusetMems = strings.Join(cpusetMems, ",")
