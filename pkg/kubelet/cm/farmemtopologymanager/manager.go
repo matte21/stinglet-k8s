cd077cd94c7b1e40deb54fddf8d8290efa2dbdd0
matte21 <matteoolivi7@gmail.com>
2025-09-22 21:05:25 -0400

    Fix nil pointer dereference

--- pkg/kubelet/cm/farmemtopologymanager/manager.go
+++ pkg/kubelet/cm/farmemtopologymanager/manager.go
@@ -1342,7 +1342,7 @@ func (m *Manager) candidateBetterThanCurrent(candidate, current []int, farMemReq
 			for zN := range m.topo.NNUMANodes[nID].NeighborZNUMAs {
 				if _, alreadySeen := zNUMAsSet[zN]; !alreadySeen {
 					zNUMAsSet[zN] = struct{}{}
-					candidateFarMem += m.topo.ZNUMANodes[nID].AllocatableBytes
+					candidateFarMem += m.topo.ZNUMANodes[zN].AllocatableBytes
 				}
 			}
 		}
@@ -1353,7 +1353,7 @@ func (m *Manager) candidateBetterThanCurrent(candidate, current []int, farMemReq
 			for zN := range m.topo.NNUMANodes[nID].NeighborZNUMAs {
 				if _, alreadySeen := zNUMAsSet[zN]; !alreadySeen {
 					zNUMAsSet[zN] = struct{}{}
-					currentFarMem += m.topo.ZNUMANodes[nID].AllocatableBytes
+					currentFarMem += m.topo.ZNUMANodes[zN].AllocatableBytes
 				}
 			}
 		}
