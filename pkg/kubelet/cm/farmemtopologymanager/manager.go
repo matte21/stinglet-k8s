c4d7ba0b730f25385263320d0153710bcb3bd925
matte21 <matteoolivi7@gmail.com>
2025-09-22 22:08:50 -0400

    Improve NUMA group selection

--- pkg/kubelet/cm/farmemtopologymanager/manager.go
+++ pkg/kubelet/cm/farmemtopologymanager/manager.go
@@ -1409,8 +1409,8 @@ func (m *Manager) candidateBetterThanCurrent(candidate, current []int, farMemReq
 		}
 	}
 
-	if len(candidate) < len(current) {
-		return true
+	if len(candidate) != len(current) {
+		return len(candidate) < len(current)
 	}
 
 	candidateDistance := uint64(0)
